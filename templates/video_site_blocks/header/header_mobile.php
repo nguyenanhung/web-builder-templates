01cf2f9f82096354b884d64edf7642b8c4bef65b
nguyenanhung <info@nguyenanhung.com>
2022-07-04 10:32:09 +0700

    Release version 1.0.3

--- templates/video_site_blocks/header/header_mobile.php
+++
@@ -1,9 +0,0 @@
-<?php if (isset($seo)) { ?>
-    <div class="header-mobile">
-        <ul class="list-items-menu-mobile">
-            <li><a title="Trang chủ" href="<?= $seo->siteUrl(); ?>" class="active"><i class="fa fa-home"></i></a></li>
-            <li><a title="Video" href="<?= $seo->siteUrl('videos') ?>"><i class="fa fa-video-camera"></i></a></li>
-            <li><a title="Gói cước" href="<?= $seo->siteUrl('users/sign-up') ?>"><i class="fa fa-user-circle-o"></i></a></li>
-        </ul>
-    </div>
-<?php } ?>
--- templates/xml/google_shopping/google_shopping.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Project web-builder-sdk
- * Created by PhpStorm
- * User: 713uk13m <dev@nguyenanhung.com>
- * Copyright: 713uk13m <dev@nguyenanhung.com>
- * Date: 06/06/2022
- * Time: 16:54
- */
